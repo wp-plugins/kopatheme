8ac07a465af68b8b798a7bd9301b43a4b8fc3af6
kopatheme <kopatheme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-15 02:36:53 +0000

    First added
    git-svn-id: https://plugins.svn.wordpress.org/kopatheme/trunk@989825 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/kopa-core-functions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Kopa Framework Core Functions
+ *
+ * General core functions available on both the front-end and admin.
+ *
+ * @author      Kopatheme
+ * @category    Core
+ * @package     KopaFramework/Functions
+ * @since       1.0.0
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Include core functions
+// formating functions
+include_once( 'kopa-formatting-functions.php' );
+
+// utility functions for getting options from 
+// theme options, sidebar & layout manager
+include_once( 'kopa-settings-functions.php' );
+
+// google fonts, system fonts utility functions
+include_once( 'kopa-fonts-functions.php' );
+
+/**
+ * Get textdomain for translation
+ * 
+ * @since 1.0.0
+ */
+function kopa_get_domain() {
+	if ( defined( 'KOPA_DOMAIN' ) ) {
+		return KOPA_DOMAIN;
+	}
+
+	return 'kopa-framework';
+}
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Kopa Framework ===
+Contributors: kopatheme
+Tags: framework, tool, feature, theme-options, sidebar-manager, layout-manager, custom-layouts
+Donate link: 
+Requires at least: 3.8
+Tested up to: 3.9
+Stable tag: 1.0.0
+License: GPLv2 or later
+
+A WordPress framework by Kopatheme
+
+== Description ==
+
+The Kopa Framework plugin is an easy way to get theme options, sidebar manager, layout manager and custom layouts feature to your WordPress site.
+
+== Installation ==
+
+1. Upload the files to the /wp-content/plugins/kopa-framework/ directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to Appearance->Theme Options and use.
+
+== Changelog ==
+
+= 1.0.0 =
+* First version
